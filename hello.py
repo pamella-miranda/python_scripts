0228749748ed5cd82575bc815714a884eb63bdb3
Pamella Miranda <129092835+pamella-miranda@users.noreply.github.com>
2023-06-09 15:35:52 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#python3 hello.py argument
+
+#import module sys which is a very standard one
+import sys
+
+#gather the code in a main() function
+def main():
+    print ('Hello there', sys.argv[1]) #sys.argv[1] calls the first argument
+                                       #sys.argv[0] is the script name itself and can be ignored
+    
+#standard boilerplate to call the main() function to begin the program
+if __name__ == '__main__':
+    main()    
