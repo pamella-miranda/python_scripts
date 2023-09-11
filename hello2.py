9b80d500966292132a7aeed49322adfa297f77a7
Pamella Miranda <129092835+pamella-miranda@users.noreply.github.com>
2023-09-11 16:29:21 -0300

    enter a name

---
+++ hello2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#python3 hello2.py
+
+print ("Hello and welcome.")
+
+name = input ("What is your name? ")
+
+if name == "Martin":
+    print ("That is my name too!")
+    
+print ("Hi " + name)
+     
+print ("Hello world!")
