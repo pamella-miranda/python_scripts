557f9f49120e3f1d33109aee5ddf1d552451cff9
Pâmella Miranda <129092835+pamella-miranda@users.noreply.github.com>
2025-11-10 09:24:31 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
-
-#python3.* hello.py argument
+#You run this script as follows:
+#
+# python3.* hello.py argument
+#
 #where * is the version of Python
 #where "argument" is your argument
 
 #import module sys which is a very standard one
 import sys
 
+#sys.argv[0] is the script name itself and can be ignored
+word = sys.argv[1] #calls the first argument
+
 #gather the code in a main() function
 def main():
-    print ('Hello there', sys.argv[1]) #sys.argv[1] calls the first argument
-                                       #sys.argv[0] is the script name itself and can be ignored
+    print ('Hello there', word) 
     
 #standard boilerplate to call the main() function to begin the program
 if __name__ == '__main__':
